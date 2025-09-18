7e7e43d96114bca3abade0961d6e5213d0faa78f
RANDOMUSER203 <zackary.msefer@gmail.com>
2025-09-18 16:35:37 -0400

    tungtung.js

--- tungtung.js
+++ tungtung.js
@@ -1,6 +1,8 @@
+src = "https://brushme.com/static/uploads/media/products/product_12927_68348a383fe072.97120940.jpg"
+
 function tungtung2() {
     document.querySelectorAll("img").forEach(img => {
-        img.src = "https://brushme.com/static/uploads/media/products/product_12927_68348a383fe072.97120940.jpg"
+        img.src = src
     })
 
     requestAnimationFrame(tungtung2)
